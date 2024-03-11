7df85af50e12eec6cc6cd6066faeebb0861178ee
Nguyen Van Tien <nguyenvantien2910@gmail.com>
2024-03-11 18:55:07 +0700

    aaaaaaaaaaaa

--- Session 06/src/ra/Test.java
+++ Session 06/src/ra/Test.java
@@ -1,2 +1,22 @@
-package ra;public class Test {
+package ra;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Moi nhap email: ");
+            String inputEmail = scanner.nextLine();
+
+            String regexEmail = "^(.+)@(.+)$";
+
+            if (inputEmail.matches(regexEmail)) {
+                System.out.println(inputEmail);
+                break;
+            } else {
+                System.out.println("Mời bạn nhập lại email đúng định dạng!");
+            }
+        }
+    }
 }
